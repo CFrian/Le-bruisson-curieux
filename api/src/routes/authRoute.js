b1bb076008eacd19cc980c51ec88e22ef4fd0ce6
Florian Costes <costes.fl@gmail.com>
2026-07-20 15:29:50 +0200

    add input validation - protect routes with requireAuth

--- api/src/routes/authRoute.js
+++ api/src/routes/authRoute.js
@@ -2,9 +2,32 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
 const requireAuth = require('../middlewares/requireAuth')
+const { body, validationResult } = require('express-validator')
+
+
+//middleware de validation de body login
+const validateLogin = [
+    body('email')
+        .notEmpty().withMessage('Champ requis')
+        .isEmail().withMessage('Email invalide')
+        .normalizeEmail(),
+    body('password')
+        .notEmpty().withMessage('Champ requis')
+        .isLength({ min: 8 }).withMessage('Minimum 8 caractères'),
+    (req, res, next) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            const error = new Error(errors.array()[0].msg)
+            error.statusCode = 400
+            return next(error)
+        }
+        next()
+    }
+]
+
 
 //Route publiques
-router.post('/login', authController.login)
+router.post('/login', validateLogin, authController.login)
 router.post('/logout', authController.logout)
 
 //Routes protégées avec access token
--- api/src/routes/projectRoute.js
+++ api/src/routes/projectRoute.js
@@ -7,12 +7,18 @@
 const express = require('express')
 const router = express.Router()
 const projectController = require('../controllers/projectController')
+const requireAuth = require('../middlewares/requireAuth')
 
+
+//Routes publiques sans token
 router.get('/', projectController.getAllProjects)
 router.get('/:id', projectController.getOneProject)
-router.post('/', projectController.createProject)
-router.patch('/:id', projectController.updateProject)
-router.delete('/:id', projectController.removeProject)
+
+
+//Routes protégées avec token
+router.post('/', requireAuth, projectController.createProject)
+router.patch('/:id', requireAuth, projectController.updateProject)
+router.delete('/:id', requireAuth, projectController.removeProject)
 
 
 module.exports = router
